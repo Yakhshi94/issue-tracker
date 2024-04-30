375e6c2f8b8bff9db9d0515c503de2e8ec8b4eca
Esmatullah <esmatullahsatari@gmail.com>
2024-04-30 12:50:12 +0430

    style active   link in navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import Link from 'next/link'
+'use client';
+import React from 'react';
+import Link from 'next/link';
 import { IoBugSharp } from "react-icons/io5";
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 
 export default function Navbar() {
+    const currentPath = usePathname();
+    
     const links = [
-        {label: "Dashboard", href: "/dashboard"},
+        {label: "Dashboard", href: "/"},
         {label: "Issue", href: "/issue"},
     ]
     
@@ -12,7 +17,16 @@ export default function Navbar() {
     <nav className="flex space-x-6 border-b mb-5 items-center py-8 h-14 px-5 text-xl">
         <Link href="/"><IoBugSharp /></Link>
         <ul className="flex space-x-5">
-            {links.map(link => <Link key={link.href} href={link.href} className="text-zinc-500 hover:text-zinc-100 transition-colors">{link.label}</Link>)}
+            {links.map(link => <Link key={link.href} 
+              href={link.href} 
+              className={classnames(
+                {
+                  'text-zinc-100': currentPath === link.href,
+                  'text-zinc-500': currentPath !== link.href,
+                  'hover:text-zinc-200 transition-colors': true,
+                }
+              )}>
+              {link.label}</Link>)}
         </ul>
     </nav>
   )
