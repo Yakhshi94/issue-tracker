dfaef87ec31b7f4937be364d30e59e3157a4abec
Yakhshi94 <esmatullahsatari@gmail.com>
2024-05-04 21:35:36 +0430

    Build the new issue page

--- app/issue/page.tsx
+++ app/issue/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { TextField, Flex, TextArea, Button } from '@radix-ui/themes'
+import Link from 'next/link'
+
 
 function issuePage() {
   return (
-    <div>issuePage</div>
+    <div className='max-w-xl space-y-3'>
+        <TextField.Root variant="surface" placeholder="Title" />
+        <TextArea placeholder="Description" />
+        <Button>Submit New Issue</Button>
+    </div>
   )
 }
 
